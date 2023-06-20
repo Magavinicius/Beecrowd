b0dac8149700ba373422cea36af180f7472a1c81
Vinícius Magalhães Batista <63367957+Magavinicius@users.noreply.github.com>
2023-06-20 20:26:06 -0300

    exercicio 2

---
+++ _1001_Extremamente Basico/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner getDados = new Scanner(System.in);
+        int num1 = getDados.nextInt();
+        int num2 = getDados.nextInt();
+
+        System.out.println("X = " + (num1 + num2));
+    }
+}
